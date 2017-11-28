859c5669d29757f6e202b66b7a8c3ce7b121ee64
Ayyappan Subramanian <ayyappangs@gmail.com>
2017-11-27 22:25:00 -0600

    Simple WPF MVVM sample to show person name and age

---
+++ WPFMVVM/WPFMVVM/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFMVVM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new PersonViewModel();
+        }
+    }
+
+    class PersonViewModel
+    {
+        public Person PersonData { get; set; }
+
+        public PersonViewModel()
+        {
+            PersonData = new Person()
+            {
+                Age = 4,
+                Name = "John"
+            };
+        }
+    }
+
+    class Person
+    {
+        public int Age { get; set; }
+
+        public string Name { get; set; }
+    }
+}
